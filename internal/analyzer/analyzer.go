f23b1a3e31e1f86d6a8070480a9d4b9206ff681d
gimye <gimyedev@gmail.com>
2026-07-16 22:02:38 +0900

    feat(#2): connect doctor command to project analyzer

---
+++ internal/analyzer/analyzer.go
@@ -0,0 +1,10 @@
+package analyzer
+
+import "github.com/Team-Shell-We/infra-doctor/internal/project"
+
+func AnalyzeProject(root string) (*project.Info, error) {
+
+	info := &project.Info{}
+
+	return info, nil
+} 
