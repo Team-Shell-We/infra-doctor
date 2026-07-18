ead90c235826876031c7bc59674979dfa911f60d
jade <syeong8663@gmail.com>
2026-07-18 13:46:10 +0900

    feat(#5): help feature

--- cmd/root.go
+++ cmd/root.go
@@ -8,6 +8,7 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
+	"github.com/Team-Shell-We/infra-doctor/utils"
 )
 
 
@@ -45,7 +46,7 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
+	rootCmd.AddCommand(utils.helpCommand(rootCmd))
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
-
 
