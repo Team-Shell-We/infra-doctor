7782f83a2ee04dc992487bdaa7116a76561c1bd2
jade <syeong8663@gmail.com>
2026-08-03 02:19:52 +0900

    root.go 오류 해결

--- cmd/root.go
+++ cmd/root.go
@@ -8,8 +8,7 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
-	"github.com/Team-Shell-We/infra-doctor/utils"
-)
+) 
 
 
 
@@ -46,7 +45,6 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.AddCommand(utils.helpCommand(rootCmd))
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
